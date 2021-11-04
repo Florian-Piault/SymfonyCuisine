dc429636ce114f9397f1bd9fa2b0dbee5410bd4d
AntoniaB21 <antonia.balluais@mail-ecv.fr>
2021-11-04 01:13:12 +0100

    add validation form field

--- src/Form/IngredientQuantityType.php
+++ src/Form/IngredientQuantityType.php
@@ -8,25 +8,14 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use App\Entity\Ingredient;
-use App\Entity\Recipe;
-use App\Form\IngredientType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class IngredientQuantityType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('quantity')
-            // ->add('ingredients', CollectionType::class, [
-            //     'entry_type' => IngredientType::class,
-            //     'entry_options' => [
-            //         'label' => false
-            //     ],
-            //     'allow_add' => true,
-            //     'allow_delete' => true,
-            //     'by_reference' => false
-            // ])
+            ->add('quantity', IntegerType::class)
             ->add('ingredients',EntityType::class,[
                 'class' => Ingredient::class,
                 'choice_label' => 'name',
