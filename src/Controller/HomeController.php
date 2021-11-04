402161dfb84cb629c9d8dda1f8080b11d3de8d24
AntoniaB21 <antonia.balluais@mail-ecv.fr>
2021-11-04 15:58:45 +0100

    add favorite recipe on home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,11 +2,18 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController {
-    public function index() {
+    public function index(UserRepository $userRepository) {
         $user = $this->getUser();
-        return $this->render('index.html.twig', ['user' => $user]);
+        // $favoriteRecipes = $userRepository->getFavoriteRecipes($user);
+
+        // dd($favoriteRecipes);
+        
+        return $this->render('index.html.twig', [
+            'user' => $user 
+        ]);
     }
 }
